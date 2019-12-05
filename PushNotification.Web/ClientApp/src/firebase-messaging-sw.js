775909fed3b1b8b263b88c66cb9d235b1175fc57
mehmet Ural <mehmet.ural@linkit.nl>
2019-12-05 14:57:03 +0100

    Service worker & web manifest are added

---
+++ PushNotification.Web/ClientApp/src/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+// See https://firebase.google.com/docs/cloud-messaging/js/receive#setting_notification_options_in_the_service_worker
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '80809731750'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
